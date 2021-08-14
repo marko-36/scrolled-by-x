7c305b7969eeed8281489123374146ea5eac7827
marko-36 <26520303+marko-36@users.noreply.github.com>
2021-08-14 23:58:59 +0200

    Jquery dependence removed

--- scrolled-by-x.js
+++ scrolled-by-x.js
@@ -1,6 +1,8 @@
+var body = document.querySelector("body");
 window.lastScrollDir = "up";
 window.lastScrollTop = window.pageYOffset;
 window.scrollTreshold = 10; //
+
 
 isBigScroll = (lastScrollTop, scrollTreshold) => {
   return (Math.abs(window.pageYOffset - lastScrollTop) > scrollTreshold) ? true : false
@@ -10,23 +12,25 @@ setScrollDir = (lastScrollTop) => {
   if (window.pageYOffset > lastScrollTop) {
     window.lastScrollDir = "down";
     console.log("down");
-    $('.lastScrollDir').text('down');
-    $('body').addClass('scrolledDown');
+    //$('.lastScrollDir').text('down');
+    document.querySelector(".lastScrollDir").textContent = 'down'    
+    body.classList.add('scrolledDown');
     return "down"
   } else {
     window.lastScrollDir = "up";
     console.log("up");    
-    $('.lastScrollDir').text('up');
-    $('body').removeClass('scrolledDown');
+    //$('.lastScrollDir').text('up');
+    document.querySelector(".lastScrollDir").textContent = 'up'
+    body.classList.remove('scrolledDown');  
     return "up"
   }
 }
 
-$(window).scroll(function() {
-  $('.pageYOffset').text(window.pageYOffset);
+window.onscroll = function() {   
+  document.querySelector(".pageYOffset").textContent = window.pageYOffset
   if (isBigScroll(lastScrollTop, 10)) {
     setScrollDir(window.lastScrollTop);
-    $('.lastScrollTop').text(window.lastScrollTop);
+    document.querySelector(".lastScrollTop").textContent = window.lastScrollTop
     window.lastScrollTop = window.pageYOffset;
   }
-});
+};
