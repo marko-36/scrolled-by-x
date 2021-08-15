a88b5e5b1e9123998d390fb55f93b6c423933f34
marko-36 <26520303+marko-36@users.noreply.github.com>
2021-08-15 07:59:49 +0200

    JQuery dependence removed + demo code marked
    JQuery dependence removed + demo code marked

--- scrolled-by-x.js
+++ scrolled-by-x.js
@@ -11,26 +11,24 @@ isBigScroll = (lastScrollTop, scrollTreshold) => {
 setScrollDir = (lastScrollTop) => {
   if (window.pageYOffset > lastScrollTop) {
     window.lastScrollDir = "down";
-    console.log("down");
-    //$('.lastScrollDir').text('down');
-    document.querySelector(".lastScrollDir").textContent = 'down'    
+    console.log("down");  //demo
+    document.querySelector(".lastScrollDir").textContent = 'down'   //demo 
     body.classList.add('scrolledDown');
     return "down"
   } else {
     window.lastScrollDir = "up";
-    console.log("up");    
-    //$('.lastScrollDir').text('up');
-    document.querySelector(".lastScrollDir").textContent = 'up'
+    console.log("up");    //demo
+    document.querySelector(".lastScrollDir").textContent = 'up' //demo
     body.classList.remove('scrolledDown');  
     return "up"
   }
 }
 
 window.onscroll = function() {   
-  document.querySelector(".pageYOffset").textContent = window.pageYOffset
+  document.querySelector(".pageYOffset").textContent = window.pageYOffset //demo
   if (isBigScroll(lastScrollTop, 10)) {
     setScrollDir(window.lastScrollTop);
-    document.querySelector(".lastScrollTop").textContent = window.lastScrollTop
+    document.querySelector(".lastScrollTop").textContent = window.lastScrollTop //demo
     window.lastScrollTop = window.pageYOffset;
   }
 };
